43040b5bed02ff2f2d0e24cd69a786132dcc6cad
Mus M <mus-m@outlook.com>
2016-09-11 12:01:01 -0400

    Fix benchmark

--- bench/bench.jl
+++ bench/bench.jl
@@ -1,22 +1,17 @@
-
-
-
-using Benchmarks, PyPlot
+using BenchmarkTools, PyPlot
 using Base.Math.@horner, ParPoly
 
-const x = 2.0
-nn = 5:25
-t_horner_split = []
-t_horner = []
-xt_horner_split = []
-xt_horner = []
 
+bench(w::Function, X) = mapreduce(x -> reinterpret(Unsigned,x), |, w(x) for x in X)
+X = randn(10_000_000)
+
+nn = 5:25
 time_h = Float64[]
 time_hs = Float64[]
 time_hss = Float64[]
 for n in nn
-    println("running bench for order $(n-1)")
-    c = randn(Float64,n)
+    println("running bench for order $n")
+    c = randn(Float64,n+1)
 
     @gensym f1
     @gensym f2
@@ -24,22 +19,23 @@ for n in nn
     @eval begin
         $f1(x) = @horner x $(c...)
         $f2(x) = @horner_split x $(c...)
-        $f3(x) = @horner_split x $(c...)
+        $f3(x) = @horner_split_simd x $(c...)
     end
     @eval begin
-        $f1($x); $f2($x); $f3($x) # precompile them
+        $f1(2.); $f2($2.); $f3($2.) # precompile them
     end
-    t_h    = @benchmark @eval $f1($x)
-    t_hs   = @benchmark @eval $f2($x)
-    t_hss  = @benchmark @eval $f3($x)
+    t_h    = @benchmark bench($f1, $X)
+    t_hs   = @benchmark bench($f2, $X) 
+    t_hss  = @benchmark bench($f3, $X)
 
-    push!(time_h,   Benchmarks.SummaryStatistics(t_h).elapsed_time_center)
-    push!(time_hs,  Benchmarks.SummaryStatistics(t_hs).elapsed_time_center)
-    push!(time_hss, Benchmarks.SummaryStatistics(t_hss).elapsed_time_center)
+    push!(time_h,   median(t_h).time)
+    push!(time_hs,  median(t_hs).time)
+    push!(time_hss, median(t_hss).time)
 end
-plot(nn-1, time_h*0.001,   label="@horner")
-plot(nn-1, time_hs*0.001,  label="@horner_split")
-plot(nn-1, time_hss*0.001, label="@horner_split_simd")
+
+plot(nn, time_h*0.000001,   label="@horner")
+plot(nn, time_hs*0.000001,  label="@horner_split")
+plot(nn, time_hss*0.000001, label="@horner_split_simd")
 
 legend(loc="upper left")
 xlabel("polynomial order")
